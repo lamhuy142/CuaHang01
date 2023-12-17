446a370c9315b674aeaebc41e50a22c809f3f2fa
lamhuy142 <147523526+lamhuy142@users.noreply.github.com>
2023-12-17 17:46:37 +0700

    nút quay lại donhang

--- admin/qldonhang/detail.php
+++ admin/qldonhang/detail.php
@@ -1,8 +1,7 @@
 <?php include("../inc/top.php"); ?>
-
+<a class="btn btn-primary text-center" href="index.php?action=xem"><i class="bi bi-arrow-90deg-left"></i></a>
 <div class="row">
     <div class="col-sm-9">
-
         <table class="table table-hover">
             <tr>
                 <th>Hình ảnh</th>
@@ -13,16 +12,16 @@
             <?php
             foreach ($donhangct as $ct) :
                 foreach ($sanpham as $p) :
-                    if ( $ct["donhang_id"] == $donhanghh["id"] && $ct["sanpham_id"] == $p["id"]) { ?>
-                            <tr>
-                                <td>
-                                    <img width="40px" class="thumnail" src="..\..\<?php echo $p['hinhanh']; ?>  ">
-                                </td>
-                                <td><?php echo $p["tensp"]; ?></td>
-                                <td><?php echo number_format($p["giaban"]); ?></td>
-                                <td><?php echo $ct["soluong"]; ?></td>
+                    if ($ct["donhang_id"] == $donhanghh["id"] && $ct["sanpham_id"] == $p["id"]) { ?>
+                        <tr>
+                            <td>
+                                <img width="40px" class="thumnail" src="..\..\<?php echo $p['hinhanh']; ?>  ">
+                            </td>
+                            <td><?php echo $p["tensp"]; ?></td>
+                            <td><?php echo number_format($p["giaban"]); ?></td>
+                            <td><?php echo $ct["soluong"]; ?></td>
 
-                            </tr>
+                        </tr>
             <?php }
                 endforeach;
             endforeach; ?>
